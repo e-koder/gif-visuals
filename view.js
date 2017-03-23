481a263b2f11fd24c99c8f5e0f1d0c1b9141fb9e
peralov <peralov@gmail.com>
2017-03-23 23:31:45 +0100

    Updated view.js

--- view.js
+++ view.js
@@ -19,7 +19,7 @@ class VisualView {
     }
 
     calculateRows(squareWidth) {
-        return Math.floor(window.innerWidth / squareWidth);
+        return Math.floor(window.innerHeight / squareWidth);
     }
 
     initGrid(images, columns) {
