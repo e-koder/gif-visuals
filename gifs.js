22d4878653c3b58780f6f0eabdbee38819fd3ddd
Filip Peralov <fperalov@gmail.com>
2017-03-25 14:09:33 +0000

    gifs update

--- gifs.js
+++ gifs.js
@@ -39,5 +39,16 @@ const gifObj = [
             "img11.jpg",
             "img12.jpg"
         ]
+    }, {
+        root: "gifs/simple/",
+        list: [
+            "s1.jpg",
+            "s2.jpg",
+            "s3.jpg",
+            "s4.jpg",
+            "s5.jpg",
+            "s6.jpg",
+            "s1.jpg"
+        ]
     }
 ]
